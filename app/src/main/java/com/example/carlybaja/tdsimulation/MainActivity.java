d25dd76d4224cd1e169845c5b29afa5679b798cb
carly.baja <cbaja@hotmail.com>
2016-05-04 16:10:14 -0500

    Revert "Show Message on Log In Button."
    This reverts commit 9c3d35c2b599fd5a245583d195e45b9367d91737.

--- app/src/main/java/com/example/carlybaja/tdsimulation/MainActivity.java
+++ app/src/main/java/com/example/carlybaja/tdsimulation/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,9 +48,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void logIn(View view){
-        Toast.makeText(getApplicationContext(),"GO TO",Toast.LENGTH_LONG).show();
     }
 }
