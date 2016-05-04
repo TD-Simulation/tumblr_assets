9c3d35c2b599fd5a245583d195e45b9367d91737
Carly BAJA <cbaja@users.noreply.github.com>
2016-05-04 16:07:22 -0500

    Show Message on Log In Button.

--- app/src/main/java/com/example/carlybaja/tdsimulation/MainActivity.java
+++ app/src/main/java/com/example/carlybaja/tdsimulation/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void logIn(View view){
+        Toast.makeText(getApplicationContext(),"GO TO",Toast.LENGTH_LONG).show();
     }
 }
